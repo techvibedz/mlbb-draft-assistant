2ff69627bd721f0d8f2c05222e4f8a90c3ba802e
houdeifa <houdeifa@pop-os.(none)>
2025-05-11 00:24:34 +0100

    Auto-sync: 2025-05-11 00:24:34

--- app/config.ts
+++ app/config.ts
@@ -1,4 +1,4 @@
 export const config = {
-  apiUrl: process.env.NEXT_PUBLIC_API_URL || 'https://mlbb-draft-assistant-laf4mojsn-houdeifas-projects.vercel.app/api',
+  apiUrl: process.env.NEXT_PUBLIC_API_URL || 'https://mlbb-draft-assistant-backend.up.railway.app',
   isProduction: process.env.NODE_ENV === 'production'
 } 
