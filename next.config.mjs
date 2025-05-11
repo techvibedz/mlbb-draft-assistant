ea538d81692346e923e3bb084a3265117aa0d62b
houdeifa <houdeifa@pop-os.(none)>
2025-05-11 01:10:29 +0100

    Push latest changes

--- next.config.mjs
+++ next.config.mjs
@@ -17,7 +17,7 @@ const nextConfig = {
   webpack: (config) => {
     config.resolve.alias = {
       ...config.resolve.alias,
-      '@': path.resolve(__dirname),
+      '@': '.',
     }
     return config
   }
